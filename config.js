c84ab708d488518bd0c34f0f4d4599da8085fc9a
sadary456 <101031532+sadary456@users.noreply.github.com>
2024-10-02 08:35:13 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ function convertToBool(text, fault = 'true') {
 
 module.exports = {
     SESSION_ID: process.env.SESSION_ID || 'XBNl0BiA#4Mky4TnzLo4O939j1NKqeXznVokWR_wLQR5DDhJdF_M', // Enter Your Session ID
-    MONGODB: process.env.MONGODB || '',    // Enter Your MongoDB URL
+    MONGODB: process.env.MONGODB || 'mongodb+srv://menuka404:167943@cluster0.dclfl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',    // Enter Your MongoDB URL
     Owner: process.env.OwnerNumber || '94758900210',    // Enter Your Owner Number
-    BotNumber: process.env.BotNumber || '94773366833'    // Enter Your Bot Number
+    BotNumber: process.env.BotNumber || '94768504703'    // Enter Your Bot Number
 };
